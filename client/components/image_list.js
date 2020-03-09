9e33f5e24b89a5a314635807591fa27fbed9b6b8
earnpat <boonyanuch.patta@gmail.com>
2020-03-09 22:23:11 +0700

    [CREATE] created meteor and followed video from udemy

---
+++ client/components/image_list.js
@@ -0,0 +1,21 @@
+// create our image list component
+// import React
+import React from "react";
+import ImageDetail from "./image_detail";
+
+const IMAGES = [
+  { title: "Pen", link: "https://dummyimage.com/600x400" },
+  { title: "Pine Tree", link: "https://dummyimage.com/600x400" },
+  { title: "Mug", link: "https://dummyimage.com/600x400" }
+];
+
+// create our component
+const ImageList = () => {
+  const RenderedImages = IMAGES.map(image => 
+    <ImageDetail key={image.title} image={image} />
+  );
+  return <ul className="media-list list-group">{RenderedImages}</ul>;
+};
+
+// export our component
+export default ImageList;
---
+++ client/main.js
@@ -0,0 +1,37 @@
+// any JS in here in automatically run for us
+
+// import the React libraly
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+import axios from "axios";
+
+import "./main.html";
+import ImageList from "./components/image_list";
+
+//create a component
+class App extends Component {
+  componentWillMount = () => {
+    console.log("555555555555");
+    axios
+      .get("https://api.imgur.com/3/gallery/hot/viral/0.json")
+      .then(result => {
+        console.log(result.data);
+      })
+      .catch(err => {
+        console.log(message.err);
+      });
+  };
+  render() {
+    return (
+      <div>
+        <ImageList />
+      </div>
+    );
+  }
+}
+
+// render this component to the screen
+Meteor.startup(() => {
+  ReactDOM.render(<App />, document.querySelector(".container"));
+});
